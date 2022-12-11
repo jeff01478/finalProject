8084d9a588dee055c5e35edfc7c544796eb997bd
PCZai0813 <z2802053323@gmail.com>
2022-12-11 23:59:07 +0800

    merge

--- nurse_bodydata3.php
+++ nurse_bodydata3.php
@@ -2,50 +2,45 @@
     session_start();
     try {
         $conn = new PDO("mysql:host=localhost;dbname=access_test", "root", "");
-        $stmt = $conn->prepare("SELECT MAX id FROM esp_test");
+        $stmt = $conn->prepare("SELECT MAX(id) FROM esp_test");
         $stmt->execute();
-        $Bf_data = $stmt->fetchAll();
+        $Bf_data = $stmt->fetchAll()[0][0];
       } catch (PDOException $e) {
         echo "資料庫連結失敗! 錯誤訊息為 " . $e->getMessage();
       }
 
-      //echo "正在等待測量數據";
+    while(true){
+        try{
+            $stmt = $conn->prepare("SELECT MAX(id) FROM esp_test");
+            $stmt->execute();
+            $Af_data = $stmt->fetchAll()[0][0];
+            if($Bf_data != $Af_data){
+                break;
+            }
+        }catch (PDOException $e) {
+            echo "資料庫連結失敗! 錯誤訊息為 " . $e->getMessage();
+        }
+    }
 
+    $remark = NULL;
+    $id_number = $_SESSION['p_id_number'];
+    
+    try{
+        $stmt = $conn->prepare("SELECT temp FROM esp_test WHERE id = ".$Af_data);
+        $stmt->execute();
+        $T = $stmt->fetchAll()[0][0];
+        if($T >= 37.5){
+            $remark = "發燒";
+        }
+
+        $stmt = $conn->prepare("SELECT m_name FROM member where id_number = '".$_SESSION['p_id_number']."'");
+        $stmt->execute();
+        $p_name = $stmt->fetchAll()[0][0];
+        
+        $stmt = $conn->prepare("UPDATE body_data SET id_number = '".$id_number."', T = '".$T."', m_name = '".$p_name."', remark = '".$remark."' WHERE remark = 'good'");
+        $stmt->execute();
+    }catch(PDOException $e){
+        echo "資料庫連結失敗! 錯誤訊息為 " . $e->getMessage();
+    }
+    echo "<script> {window.alert('已完成測量');location.href='nurse_bodydata.php'} </script>";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input type="button" value="nmsl" onclick="myFunction()"></input>
-
-    <script>
-        function myFunction() {
-            document.write("nmsl");
-        }
-    </script>
-    <input type="button" name="test" id="test" value="RUN"  onclick="<?php testfun(); ?>" /><br/>
-
-    <?php
-        function testfun()
-        {
-            echo "Your test function on button click is working";
-        //   while(true){
-        //     $stmt = $conn->prepare("SELECT MAX id FROM esp_test");
-        //     $stmt->execute();
-        //     $Af_data = $stmt->fetchAll();
-        //     if($Bf_data != $Af_data){
-        //         echo "nmsl";
-        //         break;
-        //     }
-        //     sleep(5);
-        //   }
-        }
-    ?>
-</body>
-</html>
