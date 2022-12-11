96dbe445891921ac764943d82f7a1e89001e75f3
a108510315 <jeff01478@gmail.com>
2022-12-11 23:57:10 +0800

    add auto nurse_bodydata

---
+++ nurse_bodydata4.php
@@ -0,0 +1,16 @@
+<?php
+    $date = NULL;
+    if(isset($_POST['date'])){
+        $date = $_POST['date'];
+    }
+
+    try {
+        $conn = new PDO("mysql:host=localhost;dbname=access_test", "root", "");
+        $stmt = $conn->prepare("INSERT INTO body_data(m_date, remark) VALUES ('".$date."', 'good')");
+        $stmt->execute();
+    }catch (PDOException $e) {
+        echo "資料庫連結失敗! 錯誤訊息為 " . $e->getMessage();
+    }
+
+    echo "<script> {location.href='nurse_bodydata3.php'} </script>";
+?>
